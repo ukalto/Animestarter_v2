b84810c9aff7656638f41a2ad537018561b94ec8
ukalto <maxi.gaber@gmail.com>
2021-11-20 00:12:33 +0100

    created Animestarter Version 2

---
+++ animestarter.py
@@ -0,0 +1,48 @@
+import webbrowser
+import os
+
+
+def getFiles():
+    list = os.listdir(os.getcwd() + "\Files")
+    return list
+
+
+def getInput():
+    while True:
+        try:
+            number = int(input("Enter a number from 1 to " + str(len(getFiles())) + ": "))
+            # if number > getFiles() or number < 1:
+            if not 1 <= number <= len(getFiles()):
+                raise ValueError
+            else:
+                return number
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+            # better try again... Return to the start of the loop
+            continue
+
+
+def printFiles():
+    for i, j in zip(getFiles(), range(len(getFiles()))):
+        print(str(j + 1)+" "+i)
+
+
+def main():
+    printFiles()
+    number = getInput()
+    if number == 1:
+        # Finished Animes
+        print("1")
+    if number == 2:
+        # Waiting for new episodes
+        print("2")
+    if number == 3:
+        # Episodes exist
+        print("3")
+    if number == 4:
+        # Animes to Watch
+        print("4")
+
+
+if __name__ == "__main__":
+    main()
